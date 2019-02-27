f5002152167cd9cd5b5f59d9b52467f5cd0bf3fc
Alon Tamir <alonta@radware.com>
2019-02-27 14:41:59 +0200

    es stateful, in progress...

--- elasticsearch/build.sh
+++ elasticsearch/build.sh
@@ -5,7 +5,10 @@ cd "$(dirname "$0")"
 echo "ElasticSearch build"
 ./docker/build.sh
 
-echo "ElasticSearch Deploy"
+echo "ElasticSearch Deploy starting"
+kubectl create -f ./k8s/persistent-volume.yaml
+kubectl create -f ./k8s/persistent-volume-claim.yaml
 kubectl create -f ./k8s/service_internal.yaml
 kubectl create -f ./k8s/service_exposed.yaml
 kubectl create -f ./k8s/deployment.yaml
+echo "ElasticSearch Deploy done"
--- elasticsearch/clean.sh
+++ elasticsearch/clean.sh
@@ -7,7 +7,11 @@ KUBE_IP=`sudo minikube ip`
 curl -XDELETE "http://${KUBE_IP}:30002/mydata*"
 curl -XDELETE "http://${KUBE_IP}:30002/fluentbit*"
 
-echo "ElasticSearch cleanup"
+echo "ElasticSearch cleanup start"
+set -x
 kubectl delete -f ./k8s/service_internal.yaml
 kubectl delete -f ./k8s/service_exposed.yaml
 kubectl delete -f ./k8s/deployment.yaml
+kubectl delete -f ./k8s/persistent-volume-claim.yaml
+kubectl delete -f ./k8s/persistent-volume.yaml
+echo "ElasticSearch cleanup done"
