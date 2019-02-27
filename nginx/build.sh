109fc6d344c49d333aef3c1532f82a243a4ef804
Alon Tamir <alonta@radware.com>
2019-02-27 13:39:45 +0200

    nginx stateful

--- nginx/build.sh
+++ nginx/build.sh
@@ -3,5 +3,10 @@
 cd "$(dirname "$0")"
 
 echo "NGINX Deploy"
-kubectl create -f ./k8s/service.yaml
-kubectl create -f ./k8s/deployment.yaml
+kubectl create -f ./k8s/stateless/service.yaml
+kubectl create -f ./k8s/stateless/deployment.yaml
+
+kubectl create -f ./k8s/stateful/persistent-volume.yaml
+kubectl create -f ./k8s/stateful/persistent-volume-claim.yaml
+kubectl create -f ./k8s/stateful/service.yaml
+kubectl create -f ./k8s/stateful/deployment.yaml
--- nginx/clean.sh
+++ nginx/clean.sh
@@ -3,5 +3,10 @@
 cd "$(dirname "$0")"
 
 echo "NGINX cleanup"
-kubectl delete -f ./k8s/service.yaml
-kubectl delete -f ./k8s/deployment.yaml
+kubectl delete -f ./k8s/stateless/service.yaml
+kubectl delete -f ./k8s/stateless/deployment.yaml
+
+kubectl delete -f ./k8s/stateful/service.yaml
+kubectl delete -f ./k8s/stateful/deployment.yaml
+kubectl delete -f ./k8s/stateful/persistent-volume.yaml
+kubectl delete -f ./k8s/stateful/persistent-volume-claim.yaml
