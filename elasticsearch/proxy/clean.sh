0249f435e2d4703258120f29234f2d27bb63a7e3
Alon Tamir <alonta@radware.com>
2019-03-05 10:43:26 +0200

    envoy proxy - in progress

---
+++ elasticsearch/proxy/clean.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+cd "$(dirname "$0")"
+source ../../set_env.sh
+
+echo "ElasticSearch Proxy cleanup"
+kubectl delete -f ./k8s/deployment.yaml
+kubectl delete -f ./k8s/service.yaml
+kubectl delete configmap es-proxy-config
+kubectl delete secret es-proxy-tls-secret
+
